d0b5efba56899d71a6f16aac92b0ab868a60c999
0x0hodor <vedraman40@outlook.com>
2023-06-09 18:49:00 +0530

    removed post widget and other cmnted code

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,21 +1,3 @@
-// import express from "express";
-// import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
-// import { verifyToken } from "../middleware/auth.js";
-// import { addComment } from '../controllers/posts.js';
-
-// const router = express.Router();
-
-// /* READ */
-// router.get("/", verifyToken, getFeedPosts);
-// router.get("/:userId/posts", verifyToken, getUserPosts);
-
-// /* UPDATE */
-// router.patch("/:id/like", verifyToken, likePost);
-
-// /* UPDATE */
-// router.patch("/:id/comment", verifyToken, addComment);
-
-// export default router;
 import express from "express";
 import {
   getFeedPosts,
